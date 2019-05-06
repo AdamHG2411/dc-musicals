4a9b68c93a0d8b0381ee0e759cda44f134c37664
Adam Grider <adam.h.grider@gmail.com>
2019-05-05 20:26:00 -0400

    tweaks to venue

--- src/Venue.js
+++ src/Venue.js
@@ -10,18 +10,24 @@ class Venue extends Component {
 		console.log('Venue: render');
 
 		if (this.props.venueData) {
+			let name = this.props.venueData.name ? this.props.venueData.name : 'error';
+			let address = this.props.venueData.location.street ? this.props.venueData.location.street : 'error';
+			let city = this.props.venueData.location.city ? this.props.venueData.location.city : 'error';
+			let minCost = this.props.selection.tickets.min ? this.props.selection.tickets.min : 'error';
+			let maxCost = this.props.selection.tickets.max ? this.props.selection.tickets.max : 'error';
+			let buyTix = this.props.selection.tickets.buy ? this.props.selection.tickets.buy : 'error';
 			return (
 				<div className="Venue">
 					<div className="Venue-Info">
-						<p className="Venue-Name">{this.props.venueData.name}</p>
-						<p>{this.props.venueData.location.street}</p>
-						<p>{this.props.venueData.location.city}</p>
+						<p className="Venue-Name">{name}</p>
+						<p>{address}</p>
+						<p>{city}</p>
 					</div>
 					<div className="Venue-Tickets">
 						<p className="Venue-TicketCost">
-							Approx. ${this.props.selection.tickets.min} - ${this.props.selection.tickets.max}
+							Approx. ${minCost} - ${maxCost}
 						</p>
-						<a className="Venue-GetTickets" href={`${this.props.selection.tickets.buy}`}>
+						<a className="Venue-GetTickets" href={`${buyTix}`}>
 							Get Tickets
 						</a>
 					</div>
