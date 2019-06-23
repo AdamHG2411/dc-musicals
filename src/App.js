5815a5a5e061e39490d6f122f7de309c2ac76aed
Adam Grider <adam.h.grider@gmail.com>
2019-06-23 17:43:39 -0400

    restructure

--- src/App.js
+++ src/App.js
@@ -1,90 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ListItem from './ListItem.js';
 import './App.css';
-import List from './List.js';
-import Venue from './Venue.js';
-import Musical from './Musical.js';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			performanceData: null,
-			performanceDataLoaded: false,
-			sortedData: null,
-			listedPerformances: [],
-			selection: null,
+			performanceData: [],
 			venueData: null,
 			musicalData: null
 		};
-		this.render = this.render.bind(this);
-		this.componentDidMount = this.componentDidMount.bind(this);
-		this.updateSelection = this.updateSelection.bind(this);
 	}
-	updateSelection(input) {
-		this.setState({ selection: input });
-		fetch(`https://musicals-api.herokuapp.com/api/venues/${input.venueCode}`).then((venueResult) => {
-			venueResult.json().then((venue) => {
-				fetch(`https://musicals-api.herokuapp.com/api/musicals/${input.musicalName}`).then((musicalResult) => {
-					musicalResult.json().then((musical) => {
-						this.setState({ venueData: venue, musicalData: musical });
-					});
-				});
-			});
-		});
-	}
+
 	render() {
 		console.log('App: render');
+		let performanceList;
+		if (this.state.performanceData) {
+			performanceList = (
+				<div className="App-List">
+					{this.state.performanceData.map((performance) => (
+						<ListItem {...performance} key={performance._id} />
+					))}
+				</div>
+			);
+		} else {
+			performanceList = <h2>Loading...</h2>;
+		}
 		return (
 			<div className="App">
-				<h1 className="App-Header">DC Musicals</h1>
-				<div className="App-Body">
-					<div className="App-UpcomingList">
-						<h2>Upcoming Performances</h2>
-						<List
-							listedPerformances={this.state.listedPerformances}
-							className="App-PerformanceList"
-							updateSelection={this.updateSelection}
-						/>
-					</div>
-					<div className="App-SelectedPerformance">
-						<h2>Venue</h2>
-						<Venue
-							className="App-SelectedVenue"
-							venueData={this.state.venueData}
-							musicalData={this.state.musicalData}
-							selection={this.state.selection}
-						/>
-						<h2>Musical</h2>
-						<Musical className="App-SelectedMusical" musicalData={this.state.musicalData} />
-					</div>
-				</div>
+				<header className="App-Header">
+					<h1>DC Musicals</h1>
+				</header>
+				<main className="App-Body">{performanceList}</main>
 			</div>
 		);
 	}
 	componentDidMount() {
 		console.log('App: componentDidMount');
-		fetch('https://musicals-api.herokuapp.com/api/performances/')
-			.then((res) => res.json())
-			.then((data) => {
-				this.setState({ performanceData: data });
-			})
-			.then(() => {
-				if (this.state.performanceData) {
-					this.setState({
-						sortedData: this.state.performanceData.sort(function(a, b) {
-							let keyA = new Date(a.dates.start);
-							let keyB = new Date(b.dates.start);
-							if (keyA < keyB) return -1;
-							if (keyA > keyB) return 1;
-							return 0;
-						})
-					});
-					for (let i = 0; i < this.state.sortedData.length; i++) {
-						this.state.listedPerformances.push({ performance: this.state.sortedData[i], key: i });
-					}
-					this.setState({ performanceDataLoaded: true });
-				}
-			});
+		axios.get('https://musicals-api.herokuapp.com/api/performances/').then((performances) => {
+			console.log(performances);
+			let performanceData = performances.data
+				.filter((performance) => {
+					let performanceEnd = new Date(performance.dates.end);
+					let today = new Date();
+					return performanceEnd >= today;
+				})
+				.sort(function(a, b) {
+					let keyA = new Date(a.dates.start);
+					let keyB = new Date(b.dates.start);
+					if (keyA < keyB) return -1;
+					if (keyA > keyB) return 1;
+					return 0;
+				});
+			console.log(performanceData);
+			this.setState({ performanceData });
+		});
 	}
 }
 export default App;
--- src/Musical.js
+++ src/Musical.js
@@ -1,40 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Musical.css';
 
-class Musical extends Component {
-	constructor(props) {
-		super(props);
-		this.render = this.render.bind(this);
-	}
-	render() {
-		console.log('Musical: render');
-		if (this.props.musicalData) {
-			return (
-				<div>
-					<div className="Musical">
-						<p className="Musical-Name">
-							{this.props.musicalData.name} ({this.props.musicalData.premiereYear})
-						</p>
-						<p className="Musical-Composer">{this.props.musicalData.composer}</p>
-					</div>
-					<div>
-						<h2>Tracklist</h2>
-						<iframe
-							title="SpotifyPlayer"
-							src={`https://open.spotify.com/embed/album/${this.props.musicalData.spotifyURL}`}
-							width="380"
-							height="380"
-							frameBorder="0"
-							allowtransparency="true"
-							allow="encrypted-media"
-						/>
-					</div>
-				</div>
-			);
-		} else {
-			return null;
-		}
-	}
+function Musical(props) {
+	return (
+		<div className="Musical">
+			<p>
+				{props.musicalName} ({props.musicalYear})
+			</p>
+			<p>{props.musicalComposer}</p>
+			<p>{props.venueName}</p>
+			<p>{props.venueAddress}</p>
+			<p>{props.venueCity}</p>
+			<iframe
+				title="SpotifyPlayer"
+				src={`https://open.spotify.com/embed/album/${props.musicalSpotify}`}
+				width="380"
+				height="380"
+				frameBorder="0"
+				allowtransparency="true"
+				allow="encrypted-media"
+			/>
+		</div>
+	);
 }
 
 export default Musical;
